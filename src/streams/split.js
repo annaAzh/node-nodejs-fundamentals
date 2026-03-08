e1e7fd407181c9e848e51c8c708489feffd7dad5
annaAzh <forarre@yandex.ru>
2026-03-08 20:03:07 +0300

    feat(src/streams): solve split

--- src/streams/split.js
+++ src/streams/split.js
@@ -1,8 +1,50 @@
+import { argv } from 'node:process'
+import { Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import { createReadStream, createWriteStream } from 'node:fs';
+import { resolve } from 'path';
+
+const defaultLines = 10
+
 const split = async () => {
-  // Write your code here
-  // Read source.txt using Readable Stream
-  // Split into chunk_1.txt, chunk_2.txt, etc.
-  // Each chunk max N lines (--lines CLI argument, default: 10)
+  const filePath = resolve('source.txt')
+  const pathToChunks = resolve('.')
+
+  const index = argv.indexOf('--lines')
+  const linesPerFile = (index !== -1 && index + 1) ? parseInt(argv[index + 1], 10) : defaultLines
+
+  const read = createReadStream(filePath, 'utf-8')
+
+  let fileIndex = 1;
+  let buffer = []
+
+  const transform = new Transform({
+    transform(chunk, _encoding, callback) {
+
+    const lines = chunk.toString().split(/\r?\n/)
+     for (const line of lines) {
+        buffer.push(line)
+        if(buffer.length >= linesPerFile) {
+          const filePath = resolve(pathToChunks, `chunk_${fileIndex}.txt`)
+          const ws = createWriteStream(filePath)
+          ws.write(buffer.join('\n') + '\n')
+          ws.end()
+          fileIndex += 1
+          buffer = []
+        }
+     }
+      callback()
+    },
+    flush(callback) {
+      if (buffer.length > 0) {
+        const filePath = resolve(pathToChunks, `chunk_${fileIndex}.txt`)
+        createWriteStream(filePath, buffer.join('\n'))
+      }
+      callback()
+    }
+  })
+
+  await pipeline(read, transform)
 };
 
 await split();
