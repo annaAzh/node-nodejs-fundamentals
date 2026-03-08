777de5104091076c05db6361a960b1ee2a1f7561
annaAzh <forarre@yandex.ru>
2026-03-08 16:48:53 +0300

    feat(src/wt): solve worker threads

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,9 +1,8 @@
 import { parentPort } from 'worker_threads';
 
-// Receive array from main thread
-// Sort in ascending order
-// Send back to main thread
-
 parentPort.on('message', (data) => {
-  // Write your code here
+  const res =  data.sort((a, b) => a - b)
+
+  parentPort.postMessage(res)
+  parentPort.close()
 });
